e3ecbd383244d534f168179d368e1291a73abbb9
JX518 <Xujustin7@gmail.com>
2022-07-22 08:20:15 -0700

    Minor changes were made, may be able to run on HTTPS, not sure.

--- HTTPSForeground.py
+++ HTTPSForeground.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from ssl import SSLContext
 from socket import socket
-from ssl import SSLContext as ssl
+import ssl
 import os
 import time
 
@@ -25,11 +26,13 @@ for i in range(num, len(configSplit)):
 
 host = configSplit[num].split(":")[1].replace(" ", "")
 port = 443
+#added this for shorter name but also easier searching, because there could be multiple name references to these numbers
+#implemented to easier future code maintanence
+addr = (host, port)
 
 # print("Hosting on " + str(host) + ", with port " + str(port))
 
 class HttpServer(BaseHTTPRequestHandler):
-
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -38,7 +41,17 @@ class HttpServer(BaseHTTPRequestHandler):
         file = open(folderLocation + "localhost.html", 'r')
         self.wfile.write(bytes(file.read(), "utf-8"))
 
-server = HTTPServer((host, port), HttpServer)
-server.socket
+server = HTTPServer(addr, HttpServer)
+
+#need an instance of SSL
+context = SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.check_hostname=True
+context.verify_mode=ssl.CERT_REQUIRED
+#context.load_verify_locations(folderLocation + "localhost.pem")
+
+sock = socket()
+SSLSock = context.wrap_socket(sock)
+server.socket = SSLSock
+SSLSock.connect(addr)
 server.serve_forever()
 server.close()
